d3e2c01d96f7bb12dd7c9791b874566ca30c65a3
siddhesh <iamsiddhesh22@gmail.com>
2021-04-27 22:18:56 +0530

    added coronavirus api

--- Components/Card/NumberCard/index.jsx
+++ Components/Card/NumberCard/index.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { StyledCard } from "..";
+import styled from "styled-components";
 
-export default function NumberCard() {
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+`;
+
+export default function StatsCard() {
   return (
     <>
-      <StyledCard></StyledCard>
+      <CardContainer>
+        <StyledCard>
+          <h1>lol</h1>
+        </StyledCard>
+      </CardContainer>
     </>
   );
 }
--- pages/Stats.jsx
+++ pages/Stats.jsx
@@ -1,8 +1,32 @@
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import { MainContainer } from "../Container/MainContainer";
+import { RestContainer } from "../Container/RestContainer";
+import styled from "styled-components";
+import StatsCard from "../Components/Card/NumberCard";
 
-export default function Stats() {
+const StatsCardContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+export async function getServerSideProps() {
+  const res = await fetch("https://covid19.mathdro.id/api/countries/IN");
+  const json = await res.json();
+  return {
+    props: {
+      confirmed: json.confirmed.value,
+      recovered: json.recovered.value,
+      deaths: json.deaths.value,
+    },
+  };
+}
+
+export default function Stats({ confirmed, recovered, deaths }) {
   return (
     <>
       <Head>
@@ -10,8 +34,13 @@ export default function Stats() {
         <meta name="keywords" content="Stats" />
       </Head>
       <MainContainer auto>
-        <h1>lol</h1>
-        <p>confirmed :{data}</p>
+        <h1>Stats</h1>
+        <RestContainer>
+          <StatsCardContainer>
+            <StatsCard />
+            <StatsCard />
+          </StatsCardContainer>
+        </RestContainer>
       </MainContainer>
     </>
   );
