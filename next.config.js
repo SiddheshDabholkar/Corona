928cea891227c675cdfa311114127b75d644e8b6
siddhesh <iamsiddhesh22@gmail.com>
2021-05-04 18:06:14 +0530

    added pwa in next js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const withPWA = require("next-pwa");
+
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
@@ -9,3 +11,11 @@ module.exports = {
 };
 
 module.exports = withBundleAnalyzer();
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
